2ce299410dcec51b6a0cf7dded687b10dff18502
Nishan-Kamath <nishankamath@gmail.com>
2025-11-11 15:21:58 +0530

    day1

---
+++ prgm.py
@@ -0,0 +1,18 @@
+f = ['apple','banana','cherry']
+print(f)
+
+f.append("mango")
+print(f)
+print(len(f))
+
+f.insert(1,"orange")
+print(f)
+
+f.remove("mango")
+print(f)
+
+f.pop(1)
+print(f)
+
+f.clear()
+print(f)
